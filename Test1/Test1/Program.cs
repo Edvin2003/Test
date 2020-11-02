e665ebcd1ceb488d77947dcee2f22b25d6ab0561
Elev <Elev@TEB127022>
2020-11-02 09:11:15 +0100

    Skrev bara

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -11,7 +11,7 @@ namespace Test1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.WriteLine("Hello world! 2.0"); 
+            Console.WriteLine("Vad håller du på med?"); 
         }
     }
 }
